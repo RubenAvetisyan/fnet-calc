e13ca1c13c81051d01f75f9ad16d8af1bffb8bc1
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-09-16 02:38:37 +0400

    ionic setup

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+// You might choose this based on an API call or logged-in status
+const layout = "mobile";
+</script>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
