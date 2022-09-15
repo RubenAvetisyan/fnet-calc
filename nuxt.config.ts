84cb1a8c86fe93e4f0418091a7fd3549d99f4ccd
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2022-09-15 14:38:32 +0400

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+import { defineNuxtConfig } from 'nuxt'
+
+const startUrl = process.env.NUXT_START_URL || 'http://localhost:3000/'
+
+export default defineNuxtConfig({
+  ssr: false,
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@kevinmarrec/nuxt-pwa'
+  ],
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
+  hooks: {
+    'vite:extendConfig': function (config: any, { isServer }: any) {
+      if (isServer) {
+        // Workaround for netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    },
+  },
+
+  pwa: {
+    manifest: {
+      name: 'fNet Price Calculator',
+      short_name: 'fnet-calc',
+      lang: 'en',
+      background_color: '#fff',
+      start_url: startUrl
+    },
+    icon: {
+      fileName: 'apple-icon.png',
+    },
+    workbox: {
+      enabled: true
+    }
+  }
+})
