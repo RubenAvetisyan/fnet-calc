1ae3347eb4206503bc08bb79f635072de2340699
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2022-09-16 09:52:27 +0400

    delete all

--- android/app/src/main/java/com/aks/ionicnuxtapp/MainActivity.java
+++
@@ -1,9 +0,0 @@
-<<<<<<<< HEAD:android/app/src/main/java/com/aks/ionicnuxtapp/MainActivity.java
-package com.aks.family-networf-calculator;
-========
-package com.aks.fnetcalc;
->>>>>>>> e13ca1c13c81051d01f75f9ad16d8af1bffb8bc1:android/app/src/main/java/com/aks/fnetcalc/MainActivity.java
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- server/api/getAllPosts.ts
+++
@@ -1,6 +0,0 @@
-import dbClient from '../utils';
-export default defineEventHandler(async (event) => {
-    console.log("get all posts");
-    const data = await dbClient.imagePost.findMany();
-    return  data 
-  })
