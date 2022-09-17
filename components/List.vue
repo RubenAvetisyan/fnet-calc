27341d2a17c1d8e11bf2b2abce59e4632e0bd5d2
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-09-17 14:55:48 +0400

    list view for results

---
+++ components/List.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  items: { type: Array, required: true }
+})
+</script>
+
+<template>
+  <ion-list align="content-center items-center" class="w-full text-center">
+    <ion-list-header class="text-center">
+      <slot name="headtext">
+      </slot>
+    </ion-list-header>
+  </ion-list>
+
+  <slot v-for="(item, index) in items">
+    <ion-item class="text-center">
+      <!-- <ion-avatar slot="start">
+      <img src="./avatar-finn.png" />
+    </ion-avatar> -->
+      <ion-label> {{item}} </ion-label>
+    </ion-item>
+  </slot>
+</template>
