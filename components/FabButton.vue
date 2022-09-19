9afc3943dd42e60f467b155047779f2cbebbeef2
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2022-09-19 13:52:33 +0400

    adding root syle:color and adding fab-button for accounts

---
+++ components/FabButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { personOutline } from 'ionicons/icons'
+</script>
+
+<template>
+  <ion-fab vertical="top" horizontal="end" slot="fixed">
+    <ion-fab-button color="tertiary" size="small">
+      <ion-icon :icon="personOutline"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+</template>
