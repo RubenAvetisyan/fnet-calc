c9831b253707bda1c8ffc229cd92b4fba3f4711f
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-09-20 21:04:02 +0400

    Ui omt-in

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,23 +1,26 @@
 <template>
   <ion-app align="content-center">
-    <ion-page class="ion-padding-start">
+    <ion-page class="ion-padding-start lg:px-1/9">
       <ion-header class="flex text-center items-center mb-1">
         <ion-toolbar>
           <Logos />
         </ion-toolbar>
       </ion-header>
-      <ion-content
-        class="sm:prose h-full items-center content-center text-center self-center px-10 w-full text-center ion-justify-content-center">
+      <ion-content color="light" :fullscreen="true"
+        class="ion-padding h-full items-center content-center text-center self-center px-10 w-full text-center ion-justify-content-center">
         <slot> </slot>
       </ion-content>
       <ion-footer v-if="$slots.footer" color="medium" pos="bottom-0 fixed" align="items-center content-center">
         <slot name="footer"></slot>
       </ion-footer>
-      </ion-page>
-      </ion-app>
+    </ion-page>
+    </ion-app>
 </template>
 
 <style lang="css">
+.page {
+  background: green
+}
 .header-md::after {
 
   left: 0;
