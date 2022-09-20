bb2adc0092016e602454db8a1a6f046ac7203846
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2022-09-20 18:31:49 +0400

    adding single modal component for maximum use

---
+++ components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-modal :is-open="isOpen">
+    <ion-content class="ion-padding">
+      <ion-item>
+        <slot></slot>
+      </ion-item>
+    </ion-content>
+  </ion-modal>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    deafult: false
+  }
+})
+</script>
